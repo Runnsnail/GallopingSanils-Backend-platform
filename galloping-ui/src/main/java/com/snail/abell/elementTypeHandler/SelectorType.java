de9f907ab9142a43d3b28b3e3267281246c4cd19
LAPTOP-BD1PELL5\huawei <1692178653@qq.com>
2022-11-10 21:05:19 +0800

    优化

---
+++ galloping-ui/src/main/java/com/snail/abell/elementTypeHandler/SelectorType.java
@@ -0,0 +1,20 @@
+package com.snail.abell.elementTypeHandler;
+
+/**
+ * @author Abell
+ * @date 2022/11/10
+ */
+public enum SelectorType {
+
+
+    //定位类型
+
+    ID,
+    NAME,
+    CLASS,
+    TAG,
+    LINK,
+    PARTIALLINKTEXT,
+    CSS,
+    XPATH,
+}
